908634f815b2b2f044f18c3824441327a355d28d
Lorian Coltof <loriancoltof@gmail.com>
2013-03-28 21:38:41 +0100

    moved Intersect from Util to Calculate added DyDx and fnInt functions

---
+++ lorian/graph/function/Calculate.java
@@ -0,0 +1,78 @@
+package lorian.graph.function;
+
+import java.text.DecimalFormat;
+
+public class Calculate {
+
+	
+	public static PointXY Intersect(Function f, Function g, PointXY min, double width)
+	{
+		double smallestDelta = 1000000000;
+		double currentDelta;
+		double step = 10 / 10000000.0;
+		double x = 0, x2 = 0;
+		 
+	
+		for(x = min.getX(); x < width;x+=0.1)
+		{
+			currentDelta = Math.abs(g.Calc(x) - f.Calc(x));
+			if(currentDelta < 1)
+			{
+				x2 = x;
+				break;
+			}
+		}
+		
+		for(x = x2; x < x2 + 1; x+= 0.001)
+		{
+			currentDelta = Math.abs(g.Calc(x) - f.Calc(x));
+			if(currentDelta < 0.01)
+			{
+				x2 = x;
+				break;
+			}
+		}
+		
+		for(x = x2; x < x2 + 1; x+= step)
+		{
+			currentDelta = Math.abs(g.Calc(x) - f.Calc(x));
+			if(smallestDelta > currentDelta)
+			{
+				smallestDelta = currentDelta;
+				x2 = x;
+			}
+			
+		}
+		return new PointXY(x2, f.Calc(x2));
+	}
+	
+	public static double DyDx(Function f, double x)
+	{
+		//0.0000001
+		double dx = 0.000000001;
+		double dydx = (f.Calc(x+dx) - f.Calc(x)) / dx;
+		
+		return Util.round(dydx, 6);
+	}
+	
+	public static double fnInt(Function f, double LowX, double UpX)
+	{
+		long starttime = System.currentTimeMillis();
+		double dx = 0.00001;
+		double lowersum=0, uppersum=0;
+		for(double x = LowX; x < UpX; x += dx)
+		{
+			if(x != LowX)
+			{
+				uppersum += f.Calc(x) * dx;
+			}
+			if(x != (UpX - dx))
+			{
+				lowersum += f.Calc(x) * dx;
+			}
+		}
+		double average = (lowersum + uppersum) / 2;
+		System.out.println("Passed: " + (System.currentTimeMillis() - starttime) + "ms"); 
+		return Util.round(average, 6);
+	}
+}
