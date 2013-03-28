1746cd682b733f55e15e1eeae72bcce29f7c5bfa
Lorian Coltof <loriancoltof@gmail.com>
2013-03-28 21:37:59 +0100

    added round function for rounding doubles

--- lorian/graph/function/Util.java
+++ lorian/graph/function/Util.java
@@ -21,48 +21,11 @@ public class Util {
 		}
 		return false;
 	}
-	public static PointXY Intersect(Function f, Function g, PointXY min, double width)
+	public static double round(double valueToRound, int numberOfDecimalPlaces)
 	{
-		double smallestDelta = 1000000000;
-		double currentDelta;
-		double step = (width / 10000000.0);
-		double x = 0, x2 = 0;
-		 
-	
-		for(x = min.getX(); x < width;x+=0.1)
-		{
-			currentDelta = Math.abs(g.Calc(x) - f.Calc(x));
-			if(currentDelta < 1)
-			{
-				x2 = x;
-				break;
-			}
-		}
-		
-		for(x = x2; x < x2 + 1; x+= 0.001)
-		{
-			currentDelta = Math.abs(g.Calc(x) - f.Calc(x));
-			if(currentDelta < 0.01)
-			{
-				x2 = x;
-				break;
-			}
-		}
-		
-		for(x = x2; x < x2 + 1; x+= step)
-		{
-			currentDelta = Math.abs(g.Calc(x) - f.Calc(x));
-			if(smallestDelta > currentDelta)
-			{
-				smallestDelta = currentDelta;
-				x2 = x;
-			}
-			
-		}
-		return new PointXY(x2, f.Calc(x2));
-		
-				
-		
-		
+	    double multipicationFactor = Math.pow(10, numberOfDecimalPlaces);
+	    double interestedInZeroDPs = valueToRound * multipicationFactor;
+	    return Math.round(interestedInZeroDPs) / multipicationFactor;
 	}
+	
 }
