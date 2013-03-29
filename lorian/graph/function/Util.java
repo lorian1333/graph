dae6d9273ef30b52df536535180823098f6171fc
Lorian Coltof <loriancoltof@gmail.com>
2013-03-29 14:34:38 +0100

    Added new StringContains and LowerAlphabethWithout functions used by the new function parser

--- lorian/graph/function/Util.java
+++ lorian/graph/function/Util.java
@@ -21,6 +21,37 @@ public class Util {
 		}
 		return false;
 	}
+	public static boolean StringContains(String s, String chars)
+	{
+		for(int i=0;i<chars.length();i++)
+		{
+			if(StringContains(s, chars.charAt(i))) return true;
+		}
+		return false;
+	}
+	public static String LowercaseAlphabethWithout(char character)
+	{
+		String s = "";
+		for(char ch = 'a'; ch <= 'z'; ch++)
+		{
+			if(ch != character) s += ch;
+		}
+		return s;
+	}
+	public static int StringIndexNotOf(String s, String chars)
+	{
+		int i;
+		char ch;
+		for(i=0;i<s.length();i++)
+		{
+			ch = s.charAt(i);
+			if(!StringContains(chars, ch))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
 	public static double round(double valueToRound, int numberOfDecimalPlaces)
 	{
 	    double multipicationFactor = Math.pow(10, numberOfDecimalPlaces);
