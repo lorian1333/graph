e71a0313a5afd2d5e4f7d45eb7267b727e2abc18
Lorian Coltof <loriancoltof@gmail.com>
2013-03-30 21:30:27 +0100

    Added fourth root function

--- lorian/graph/function/MathFunctions.java
+++ lorian/graph/function/MathFunctions.java
@@ -8,6 +8,7 @@ public class MathFunctions {
 	public static double Calculate(String functionname, List<String> args, double value)
 	{	
 		Function f = new Function(); 
+		if(args.size()<1) return 0;
 		if(functionname.equalsIgnoreCase("sin"))
 		{
 			if(!f.Parse(args.get(0))) return 0;
@@ -32,6 +33,7 @@ public class MathFunctions {
 			}
 			else //logBASE
 			{
+				if(args.size()<2) return 0;
 				Function g = new Function();
 				if(!f.Parse(args.get(1)) || !g.Parse(args.get(0)))
 					return 0;
@@ -59,17 +61,29 @@ public class MathFunctions {
 			if(!f.Parse(args.get(0))) return 0;
 			return Math.cbrt(f.Calc(value));
 		}
+		else if(functionname.equalsIgnoreCase("4rt"))
+		{
+			if(!f.Parse(args.get(0))) return 0;
+			return Math.pow(f.Calc(value), 1/4);
+		}
 		else if(functionname.equalsIgnoreCase("root"))
 		{
+			if(args.size()<2) return 0;
 			Function g = new Function();
 			if(!f.Parse(args.get(1)) || !g.Parse(args.get(0))) return 0;
 			return Math.pow(f.Calc(value), 1 / g.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("pow"))
 		{
+			if(args.size()<2) return 0;
 			Function g = new Function();
 			if(!f.Parse(args.get(0)) || !g.Parse(args.get(1))) return 0;
 			return Math.pow(f.Calc(value), g.Calc(value));
+		}
+		else if(functionname.equalsIgnoreCase("abs"))
+		{
+			if(!f.Parse(args.get(0))) return 0;
+			return Math.abs(f.Calc(value));
 		}
 		else if(functionname.charAt(0) == 'y')
 		{
@@ -86,6 +100,31 @@ public class MathFunctions {
 			if(GraphFunctionsFrame.functions.size() <= funcindex) return 0;
 			return GraphFunctionsFrame.functions.get(funcindex).Calc(f.Calc(value)); 
 		}
+		else if(functionname.equalsIgnoreCase("dydx") || functionname.equalsIgnoreCase("deriv"))
+		{
+			String otherfunction = args.get(0);
+			if(otherfunction.contains("dydx")|| otherfunction.contains("deriv")) return 0;
+			
+			if(otherfunction.charAt(0) != 'y' || otherfunction.endsWith(")")) 
+			{
+				if(!f.Parse(otherfunction)) return 0;
+				return Calculate.DyDx(f, value);
+			}
+			int funcindex;
+			try
+			{
+				funcindex = Integer.parseInt(otherfunction.substring(1)) - 1;
+			}
+			catch (NumberFormatException e)
+			{
+				return 0;
+			}
+			if(GraphFunctionsFrame.functions.size() <= funcindex) return 0;
+			
+			
+			return Calculate.DyDx(GraphFunctionsFrame.functions.get(funcindex), value);
+		}
+		
 		return 0;
 	}
 }
