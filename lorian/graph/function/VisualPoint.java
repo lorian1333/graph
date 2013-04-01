5e0055ddbd7aa06d34eaa11d4912aeb7a5aed150
Lorian Coltof <loriancoltof@gmail.com>
2013-04-01 22:39:52 +0200

    Addad LocationChangedListener to VisualPoint

--- lorian/graph/function/VisualPoint.java
+++ lorian/graph/function/VisualPoint.java
@@ -1,37 +1,49 @@
 package lorian.graph.function;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VisualPoint {
 	private PointXY point;
 	private boolean movable;
 	private boolean showCoordinates;
 	private String label;
 	private int functionIndex;
+	private List<VisualPointLocationChangeListener> listeners;
+	
 	public VisualPoint(PointXY p, boolean movable)
 	{
+		listeners = new ArrayList<VisualPointLocationChangeListener>();
 		setPoint(p);
 		setMovable(movable);
 		setShowCoordinates(false);
 		setLabel("");
 		setFunctionIndex(-1);
+		
 	}
 	public VisualPoint(PointXY p, int funcindex, boolean movable)
 	{
+		listeners = new ArrayList<VisualPointLocationChangeListener>();
 		setPoint(p);
 		setMovable(movable);
 		setShowCoordinates(false);
 		setLabel("");
 		setFunctionIndex(funcindex);
+		
 	}
 	public VisualPoint(PointXY p, int funcindex,  boolean movable, boolean showCoordinates)
 	{
+		listeners = new ArrayList<VisualPointLocationChangeListener>();
 		setPoint(p);
 		setMovable(movable);
 		setShowCoordinates(showCoordinates);
 		setFunctionIndex(funcindex);
 		setLabel("");
+		
 	}
 	public VisualPoint(PointXY p, int funcindex, boolean movable, boolean showCoordinates, String label)
 	{
+		listeners = new ArrayList<VisualPointLocationChangeListener>();
 		setPoint(p);
 		setMovable(movable);
 		setShowCoordinates(showCoordinates);
@@ -43,6 +55,21 @@ public class VisualPoint {
 	}
 	public void setPoint(PointXY point) {
 		this.point = point;
+		
+		for(VisualPointLocationChangeListener l: listeners)
+		{
+			l.OnLocationChange(this);
+		}
+	}
+	public void setPoint(PointXY point, boolean informListeners) {
+		this.point = point;
+		if(informListeners)
+		{
+			for(VisualPointLocationChangeListener l: listeners)
+			{
+				l.OnLocationChange(this);
+			}
+		}
 	}
 	public boolean isMovable() {
 		return movable;
@@ -68,4 +95,9 @@ public class VisualPoint {
 	public void setFunctionIndex(int functionIndex) {
 		this.functionIndex = functionIndex;
 	}
+	public void addLocationChangedListener(VisualPointLocationChangeListener listener)
+	{
+		this.listeners.add(listener);
+	}
 }
+
