2e406beab62fa021390f2135e1b1c1544e923173
Lorian Coltof <loriancoltof@gmail.com>
2013-04-06 17:39:14 +0200

    Fixed bug in functionExists function

--- lorian/graph/function/MathFunctions.java
+++ lorian/graph/function/MathFunctions.java
@@ -10,7 +10,7 @@ public class MathFunctions {
 	public static boolean functionExists(String functionname)
 	{
 		// Exceptions where the test below does not apply
-		if(functionname.equalsIgnoreCase("log") || functionname.equalsIgnoreCase("const")) return true;
+		if(functionname.equalsIgnoreCase("log") || functionname.equalsIgnoreCase("const") || functionname.toLowerCase().charAt(0) == 'y') return true;
 		
 		List<String> args = new ArrayList<String>();
 		args.add("1");
@@ -22,69 +22,69 @@ public class MathFunctions {
 	public static double Calculate(String functionname, List<String> args, double value)
 	{	
 		Function f = new Function(); 
-		if(args.size()<1) return 0;
+		if(args.size()<1) return Double.NaN;
 		
 		//Goniometric functions
 		if(functionname.equalsIgnoreCase("sin"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.sin(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("cos"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.cos(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("tan") || functionname.equalsIgnoreCase("tg"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.tan(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("sec"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return 1 / Math.cos(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("csc") || functionname.equalsIgnoreCase("cosec"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return 1 / Math.sin(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("cot"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return 1 / Math.tan(f.Calc(value));
 		}
 		
 		// Cyclometric functions
 		else if(functionname.equalsIgnoreCase("asin") || functionname.equalsIgnoreCase("arcsin") || functionname.equalsIgnoreCase("bgsin"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.asin(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("acos") || functionname.equalsIgnoreCase("arccos") || functionname.equalsIgnoreCase("bgcos"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.acos(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("atan") || functionname.equalsIgnoreCase("arctan") || functionname.equalsIgnoreCase("bgtan"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.atan(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("acot") || functionname.equalsIgnoreCase("arccot") || functionname.equalsIgnoreCase("bgcot"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return (Math.PI / 2) - Math.atan(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("asec") || functionname.equalsIgnoreCase("arcsec") || functionname.equalsIgnoreCase("bgsec"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.acos(1 / f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("acsc") || functionname.equalsIgnoreCase("arccsc") || functionname.equalsIgnoreCase("bgcsc"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return (Math.PI / 2) - Math.acos(1 / f.Calc(value));
 		}
 		
@@ -92,32 +92,32 @@ public class MathFunctions {
 		
 		else if(functionname.equalsIgnoreCase("sinh"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.sinh(f.Calc(value)); 
 		}
 		else if(functionname.equalsIgnoreCase("cosh"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.cosh(f.Calc(value)); 
 		}
 		else if(functionname.equalsIgnoreCase("tanh"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.tanh(f.Calc(value)); 
 		}
 		else if(functionname.equalsIgnoreCase("coth"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.cosh(f.Calc(value)) / Math.sinh(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("sech"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return 1 / Math.cosh(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("csch"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return 1 / Math.sinh(f.Calc(value));
 		}
 		
@@ -126,21 +126,21 @@ public class MathFunctions {
 		{
 			if(args.size()==1) //10log;
 			{
-				if(!f.Parse(args.get(0))) return 0;
+				if(!f.Parse(args.get(0))) return Double.NaN;
 				return Math.log10(f.Calc(value));
 			}
 			else //logBASE
 			{
-				if(args.size()<2) return 0;
+				if(args.size()<2) return Double.NaN;
 				Function g = new Function();
 				if(!f.Parse(args.get(1)) || !g.Parse(args.get(0)))
-					return 0;
+					return Double.NaN;
 				return (Math.log10(f.Calc(value)) / Math.log10(g.Calc(value)));
 			}
 		}
 		else if(functionname.equalsIgnoreCase("ln"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.log(f.Calc(value));
 		}
 		
@@ -155,40 +155,40 @@ public class MathFunctions {
 		// Roots
 		else if(functionname.equalsIgnoreCase("sqrt"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.sqrt(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("cbrt"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.cbrt(f.Calc(value));
 		}
 		else if(functionname.equalsIgnoreCase("4rt"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.pow(f.Calc(value), 1/4);
 		}
 		else if(functionname.equalsIgnoreCase("root"))
 		{
-			if(args.size()<2) return 0;
+			if(args.size()<2) return Double.NaN;
 			Function g = new Function();
-			if(!f.Parse(args.get(1)) || !g.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(1)) || !g.Parse(args.get(0))) return Double.NaN;
 			return Math.pow(f.Calc(value), 1 / g.Calc(value));
 		}
 		
 		// Power
 		else if(functionname.equalsIgnoreCase("pow"))
 		{
-			if(args.size()<2) return 0;
+			if(args.size()<2) return Double.NaN;
 			Function g = new Function();
-			if(!f.Parse(args.get(0)) || !g.Parse(args.get(1))) return 0;
+			if(!f.Parse(args.get(0)) || !g.Parse(args.get(1))) return Double.NaN;
 			return Math.pow(f.Calc(value), g.Calc(value));
 		}
 		
 		// Absolute value
 		else if(functionname.equalsIgnoreCase("abs"))
 		{
-			if(!f.Parse(args.get(0))) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
 			return Math.abs(f.Calc(value));
 		}
 		
@@ -202,10 +202,10 @@ public class MathFunctions {
 			}
 			catch (NumberFormatException e)
 			{
-				return 0;
+				return Double.NaN;
 			}
-			if(!f.Parse(args.get(0))) return 0;
-			if(GraphFunctionsFrame.functions.size() <= funcindex) return 0;
+			if(!f.Parse(args.get(0))) return Double.NaN;
+			if(GraphFunctionsFrame.functions.size() <= funcindex) return Double.NaN;
 			return GraphFunctionsFrame.functions.get(funcindex).Calc(f.Calc(value)); 
 		}
 		
@@ -217,7 +217,7 @@ public class MathFunctions {
 			
 			if(otherfunction.charAt(0) != 'y' || otherfunction.endsWith(")")) 
 			{
-				if(!f.Parse(otherfunction)) return 0;
+				if(!f.Parse(otherfunction)) return Double.NaN;
 				return Calculate.DyDx(f, value);
 			}
 			int funcindex;
@@ -227,9 +227,9 @@ public class MathFunctions {
 			}
 			catch (NumberFormatException e)
 			{
-				return 0;
+				return Double.NaN;
 			}
-			if(GraphFunctionsFrame.functions.size() <= funcindex) return 0;
+			if(GraphFunctionsFrame.functions.size() <= funcindex) return Double.NaN;
 			
 			
 			return Calculate.DyDx(GraphFunctionsFrame.functions.get(funcindex), value);
