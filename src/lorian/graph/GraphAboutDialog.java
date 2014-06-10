e8dc504d18f267c1f9d063d96b08c5248207c045
Lorian Coltof <loriancoltof@gmail.com>
2014-06-10 19:17:57 +0200

    - Made license window nicer - Fixed bug where the manifest file couldn't be read in Linux

--- src/lorian/graph/GraphAboutDialog.java
+++ src/lorian/graph/GraphAboutDialog.java
@@ -11,10 +11,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Enumeration;
 import java.util.GregorianCalendar;
+import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -54,14 +57,13 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 
 	private static boolean isopen = false;
 	private String buttonname_spacer;
-	private static Image loadImage(int width, int height, String path,
-			boolean inJar) {
+
+	private static Image loadImage(int width, int height, String path, boolean inJar) {
 		Image newImage = null;
 
 		try {
 			if (inJar) {
-				newImage = new Image(null,
-						GraphFunctionsFrame.class.getResourceAsStream(path));
+				newImage = new Image(null, GraphFunctionsFrame.class.getResourceAsStream(path));
 			} else {
 				newImage = new Image(null, path);
 			}
@@ -73,8 +75,7 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 			return null;
 		}
 
-		Image scaledImage = new Image(null, newImage.getImageData().scaledTo(
-				width, height));
+		Image scaledImage = new Image(null, newImage.getImageData().scaledTo(width, height));
 		return scaledImage;
 	}
 
@@ -83,29 +84,24 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 		if (isopen)
 			return;
 		isopen = true;
-
+		
 		shell = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
 
 		shell.setImages(parent.getImages());
 		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS)
 			shell.setSize(425, 295);
-		// shell.setSize(400, 315);
 		else
-			shell.setSize(425, 305);
+			shell.setSize(425, 350);
 
-		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_MAC
-				|| GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) {
-			java.awt.Rectangle clientArea = GraphicsEnvironment
-					.getLocalGraphicsEnvironment().getScreenDevices()[0]
-					.getDefaultConfiguration().getBounds();
+		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_MAC || GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) {
+			java.awt.Rectangle clientArea = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0].getDefaultConfiguration().getBounds();
 			Rectangle rect = shell.getBounds();
 			int x = clientArea.x + (clientArea.width - rect.width) / 2;
 			int y = clientArea.y + (clientArea.height - rect.height) / 2;
 			shell.setLocation(x, y);
 		}
 
-		shell.setText(String.format(
-				GraphFunctionsFrame.localize("about.title"), "Graph"));
+		shell.setText(String.format(GraphFunctionsFrame.localize("about.title"), "Graph"));
 
 		GridLayout gridlayout = new GridLayout();
 		gridlayout.numColumns = 1;
@@ -121,45 +117,37 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 		Label program_icon_label = new Label(shell, SWT.NONE);
 		program_icon_label.setImage(program_icon);
 		program_icon_label.setSize(48, 48);
-		program_icon_label.setLayoutData(new GridData(SWT.CENTER, SWT.TOP,
-				true, false));
+		program_icon_label.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
 		Label name = new Label(shell, SWT.NONE);
 		name.setText("\nGraph\n");
-		Font bold = new Font(parent.getDisplay(), new FontData(name.getFont()
-				.getFontData()[0].getName(),
-				name.getFont().getFontData()[0].getHeight(), SWT.BOLD));
+		Font bold = new Font(parent.getDisplay(), new FontData(name.getFont().getFontData()[0].getName(), name.getFont().getFontData()[0].getHeight(), SWT.BOLD));
 		name.setFont(bold);
 		name.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
 		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS)
-			new Label(shell, SWT.NONE).setLayoutData(new GridData(SWT.CENTER,
-					SWT.TOP, true, false));
+			new Label(shell, SWT.NONE).setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
 		Label version = new Label(shell, SWT.NONE);
 		version.setText(GraphFunctionsFrame.version + "\n");
 		version.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
 		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS)
-			new Label(shell, SWT.NONE).setLayoutData(new GridData(SWT.CENTER,
-					SWT.TOP, true, false));
+			new Label(shell, SWT.NONE).setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
-		String description_s = GraphFunctionsFrame
-				.localize("about.description");
+		String description_s = GraphFunctionsFrame.localize("about.description");
 		if (description_s.contains("\n")) {
 
 			String[] lines = description_s.split("\n");
 			for (String line : lines) {
 				Label description = new Label(shell, SWT.NONE);
 				description.setText(line);
-				description.setLayoutData(new GridData(SWT.CENTER, SWT.TOP,
-						true, false));
+				description.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 			}
 		} else {
 			Label description = new Label(shell, SWT.NONE);
 			description.setText(description_s);
-			description.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true,
-					false));
+			description.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 		}
 		new Label(shell, SWT.NONE).setText("\n");
 
@@ -168,31 +156,24 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) {
 			smallfont_size = copyright.getFont().getFontData()[0].getHeight() - 2;
 		}
-		Font small = new Font(parent.getDisplay(),
-				new FontData(copyright.getFont().getFontData()[0].getName(),
-						smallfont_size, SWT.NONE));
+		Font small = new Font(parent.getDisplay(), new FontData(copyright.getFont().getFontData()[0].getName(), smallfont_size, SWT.NONE));
 		copyright.setFont(small);
-		copyright.setText(String.format("Copyright %c 2013-%d Lorian Coltof \n",
-				0xA9, GraphFunctionsFrame.version_year));
+		copyright.setText(String.format("Copyright %c 2013-%d Lorian Coltof \n", 0xA9, GraphFunctionsFrame.version_year));
 		copyright.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
-
+		try
+		{
 		Label build_info = new Label(shell, SWT.NONE);
 		build_info.setFont(small);
 		Calendar built_date = getBuiltDate();
 		SimpleDateFormat format = new SimpleDateFormat("dd MMMMM yyyy HH:mm:ss", GraphFunctionsFrame.getCurrentLocale());
-		
-		//build_info.setText(String.format(GraphFunctionsFrame.Translate("about.buildinfo"), 
-		//		getManifestAttributeValue("Built-By"), built_date.getDate(), "maand", built_date.getYear()+1900, 
-		//				built_date.getHours(), built_date.getMinutes(), built_date.getSeconds()
-		//		));
-		
-		build_info.setText(String.format(GraphFunctionsFrame.localize("about.buildinfo"), 
-				getManifestAttributeValue("Built-By"), format.format(built_date.getTime())
-				));
-		
-		build_info.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
 
-		
+		build_info.setText(String.format(GraphFunctionsFrame.localize("about.buildinfo"), getManifestAttributeValue("Built-By"), format.format(built_date.getTime())));
+		build_info.setLayoutData(new GridData(SWT.CENTER, SWT.TOP, true, false));
+		}
+		catch(IllegalArgumentException e)
+		{
+			System.err.println("Error reading data from MANIFEST.MF");
+		}
 		buttonname_spacer = "   ";
 		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_LINUX)
 			buttonname_spacer = "    ";
@@ -205,18 +186,9 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 		layout.fill = false;
 		layout.justify = true;
 		buttons.setLayout(layout);
-		/*
-		Button credits = new Button(buttons, SWT.PUSH);
-		credits.setText(buttonname_spacer
-				+ GraphFunctionsFrame.Translate("about.credits")
-				+ buttonname_spacer);
-		credits.setData("credits");
-		credits.addSelectionListener(this);
-		*/
+		
 		Button license = new Button(buttons, SWT.PUSH);
-		license.setText(buttonname_spacer
-				+ GraphFunctionsFrame.localize("about.license")
-				+ buttonname_spacer);
+		license.setText(buttonname_spacer + GraphFunctionsFrame.localize("about.license") + buttonname_spacer);
 		license.setData("license");
 		license.addSelectionListener(this);
 
@@ -229,14 +201,12 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 		}
 
 		Button close = new Button(buttons, SWT.PUSH | SWT.RIGHT_TO_LEFT);
-		close.setText(buttonname_spacer
-				+ GraphFunctionsFrame.localize("buttons.close")
-				+ buttonname_spacer + " ");
+		close.setText(buttonname_spacer + GraphFunctionsFrame.localize("buttons.close") + buttonname_spacer + " ");
 		close.addSelectionListener(this);
 		close.setData("close");
 		close.forceFocus();
 
-		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS)
+//		if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS)
 			shell.pack();
 
 		shell.open();
@@ -262,67 +232,51 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 	@Override
 	public void widgetSelected(SelectionEvent e) {
 		/*
-		if (((Button) e.getSource()).getData() == "credits") {
-			creditsShell = new Shell(shell, SWT.DIALOG_TRIM
-					| SWT.APPLICATION_MODAL);
-			creditsShell.setSize(450, 200);
-
-			if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_MAC
-					|| GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) {
-				creditsShell
-						.setLocation(
-								shell.getLocation().x
-										+ (shell.getSize().x - creditsShell
-												.getSize().x) / 2,
-								shell.getLocation().y + 30);
-			}
-
-			creditsShell.open();
-			creditsShell.forceActive();
-			while (!creditsShell.isDisposed()) {
-				if (!getParent().getDisplay().readAndDispatch())
-					getParent().getDisplay().sleep();
-			}
-		} else */
+		 * if (((Button) e.getSource()).getData() == "credits") { creditsShell =
+		 * new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+		 * creditsShell.setSize(450, 200);
+		 * 
+		 * if (GraphFunctionsFrame.OS ==
+		 * GraphFunctionsFrame.OperatingSystem.OS_MAC || GraphFunctionsFrame.OS
+		 * == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) { creditsShell
+		 * .setLocation( shell.getLocation().x + (shell.getSize().x -
+		 * creditsShell .getSize().x) / 2, shell.getLocation().y + 30); }
+		 * 
+		 * creditsShell.open(); creditsShell.forceActive(); while
+		 * (!creditsShell.isDisposed()) { if
+		 * (!getParent().getDisplay().readAndDispatch())
+		 * getParent().getDisplay().sleep(); } } else
+		 */
 		if (((Button) e.getSource()).getData() == "license") {
-			licenseShell = new Shell(shell, SWT.DIALOG_TRIM
-					| SWT.APPLICATION_MODAL | SWT.RESIZE);
+			licenseShell = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL | SWT.RESIZE);
 			licenseShell.setSize(650, 430);
-			licenseShell
-					.setText(GraphFunctionsFrame.localize("about.license"));
+			
+			licenseShell.setText(GraphFunctionsFrame.localize("about.license"));
 			licenseShell.setImages(shell.getImages());
 
-			if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_MAC
-					|| GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) {
-				licenseShell.setLocation(
-								shell.getLocation().x + (shell.getSize().x - licenseShell.getSize().x) / 2,
-								shell.getLocation().y + 30);
+			if (GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_MAC || GraphFunctionsFrame.OS == GraphFunctionsFrame.OperatingSystem.OS_WINDOWS) {
+				licenseShell.setLocation(shell.getLocation().x + (shell.getSize().x - licenseShell.getSize().x) / 2, shell.getLocation().y + 30);
 			}
 			GridLayout licenseLayout = new GridLayout(1, false);
 			licenseLayout.marginHeight = 10;
 			licenseLayout.marginWidth = 10;
 			licenseShell.setLayout(licenseLayout);
-			Text licenseText = new Text(licenseShell, SWT.MULTI | SWT.READ_ONLY
-					| SWT.BORDER | SWT.V_SCROLL);
+			Text licenseText = new Text(licenseShell, SWT.MULTI | SWT.READ_ONLY | SWT.BORDER | SWT.V_SCROLL);
 			licenseText.setText(getLicenseText());
 			licenseText.setBackground(new Color(null, 0xff, 0xff, 0xff));
-			licenseText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
-					true));
+			licenseText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 			FontData[] fd = licenseText.getFont().getFontData();
-			for(int i=0; i < fd.length; i++)
-			{
+			for (int i = 0; i < fd.length; i++) {
 				fd[i].setName("consolas");
 			}
 			licenseText.setFont(new Font(Display.getCurrent(), fd));
-			Composite closeButtonComp = new Composite(licenseShell,
-					SWT.RIGHT_TO_LEFT);
-			closeButtonComp.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM,
-					true, false));
+			Composite closeButtonComp = new Composite(licenseShell, SWT.RIGHT_TO_LEFT);
+			closeButtonComp.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, true, false));
 			closeButtonComp.setLayout(new GridLayout(1, true));
 
 			Button closeButton = new Button(closeButtonComp, SWT.PUSH);
 			closeButton.setText(buttonname_spacer + GraphFunctionsFrame.localize("buttons.close") + buttonname_spacer);
-			GridData data = new GridData(SWT.FILL, SWT.FILL, false, true);	
+			GridData data = new GridData(SWT.FILL, SWT.FILL, false, true);
 			closeButton.setLayoutData(data);
 			closeButton.addSelectionListener(new SelectionAdapter() {
 				@Override
@@ -331,15 +285,17 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 					licenseShell.dispose();
 				}
 			});
+			licenseShell.pack();
+			
+			if(licenseShell.getSize().y > 430)
+				licenseShell.setSize(licenseShell.getSize().x, 430); 
 			licenseShell.open();
 			licenseShell.forceActive();
 			while (!licenseShell.isDisposed()) {
 				if (!getParent().getDisplay().readAndDispatch())
 					getParent().getDisplay().sleep();
 			}
-		}
-		else if (((Button) e.getSource()).getData() == "close")
-		{
+		} else if (((Button) e.getSource()).getData() == "close") {
 			shell.dispose();
 		}
 	}
@@ -347,7 +303,7 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 	private static String getLicenseText() {
 		try {
 			InputStream is = GraphAboutDialog.class.getResourceAsStream("/res/LICENSE.md");
-			BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8"))); 
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
 
 			String license = "", line = "";
 			while ((line = br.readLine()) != null) {
@@ -360,52 +316,74 @@ public class GraphAboutDialog extends Dialog implements SelectionListener {
 			return "<error reading license file>";
 		}
 	}
-	private static String getManifestAttributeValue(String attribute)
-	{
-		Class<GraphAboutDialog> clazz = GraphAboutDialog.class;
-		/*
-		String className = clazz.getSimpleName() + ".class";
-		String classPath = clazz.getResource(className).toString();
-		
-		if (!classPath.startsWith("jar")) {
-			System.err.println("Error getting manifest attribute value: not a JAR file");
-			return "<error>";
-		} 
-		*/
-		Manifest manifest;
+
+	private static Manifest getManifest(Class<?> cl) {
+		InputStream inputStream = null;
 		try {
-			manifest = new Manifest(clazz.getResourceAsStream("/META-INF/MANIFEST.MF"));
-			Attributes attr = manifest.getMainAttributes(); 
-			String value = attr.getValue(attribute);
-			if(value == null)
-				throw new IllegalArgumentException("Attribute '" + attribute + "' was not found in MANIFEST.MF");
-			return value;
-		} catch (MalformedURLException e) {
-			e.printStackTrace(); 
-		} catch (IOException e) {
+			URLClassLoader classLoader = (URLClassLoader) cl.getClassLoader();
+			String classFilePath = cl.getName().replace('.', '/') + ".class";
+			URL classUrl = classLoader.getResource(classFilePath);
+			if (classUrl == null)
+				return null;
+			String classUri = classUrl.toString();
+			if (!classUri.startsWith("jar:"))
+				return null;
+			int separatorIndex = classUri.lastIndexOf('!');
+			if (separatorIndex <= 0)
+				return null;
+			String manifestUri = classUri.substring(0, separatorIndex + 2) + "META-INF/MANIFEST.MF";
+			URL url = new URL(manifestUri);
+			inputStream = url.openStream();
+			return new Manifest(inputStream);
+		} catch (Throwable e) {
 			e.printStackTrace();
+
+			return null;
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (Throwable e) {
+					// ignore
+				}
+			}
 		}
-		return "<error>";
-		
 	}
 
-	private static Calendar getBuiltDate()
-	{
+	private static String getManifestAttributeValue(String attribute) {
+		//try {
+			Class<GraphAboutDialog> clazz = GraphAboutDialog.class;
+
+			Manifest manifest = getManifest(clazz);
+			if(manifest == null)
+				throw new IllegalArgumentException("Attribute '" + attribute + "' was not found in MANIFEST.MF");
+			
+			Attributes attr = manifest.getMainAttributes();
+
+			String value = attr.getValue(attribute);
+			if (value == null)
+				throw new IllegalArgumentException("Attribute '" + attribute + "' was not found in MANIFEST.MF");
+			return value;
+			
+			 /*	}catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		return "<error>"; */
+
+	}
+
+	private static Calendar getBuiltDate() {
 		String builtdate = getManifestAttributeValue("Built-Date");
 		String[] tmp = builtdate.split(" ");
 		String[] date_split = tmp[0].split("-");
 		String[] time_split = tmp[1].split(":");
-		return new GregorianCalendar(
-				Integer.parseInt(date_split[2]),
-				Integer.parseInt(date_split[1]),
-				Integer.parseInt(date_split[0]),
-				Integer.parseInt(time_split[0]),
-				Integer.parseInt(time_split[1]),
-				Integer.parseInt(time_split[2])
-				);
+		return new GregorianCalendar(Integer.parseInt(date_split[2]), Integer.parseInt(date_split[1]), Integer.parseInt(date_split[0]), Integer.parseInt(time_split[0]), Integer.parseInt(time_split[1]), Integer.parseInt(time_split[2]));
 	}
-	
+
 	@Override
-	public void widgetDefaultSelected(SelectionEvent e) {}
+	public void widgetDefaultSelected(SelectionEvent e) {
+	}
 
 }
